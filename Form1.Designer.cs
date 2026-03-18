e392cd10fc767ff17589301108ea58894dc5fd02
gtapiero <giovanny.tapiero@pi.edu.co>
2026-03-18 10:28:47 -0500

    commit - player .

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -20,10 +20,13 @@
         private void InitializeComponent()
         {
             webView1 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            btnNaruto = new Button();
+            btnAttackOnTitan = new Button();
             ((System.ComponentModel.ISupportInitialize)webView1).BeginInit();
             SuspendLayout();
-
+            // 
             // webView1
+            // 
             webView1.AllowExternalDrop = true;
             webView1.CreationProperties = null;
             webView1.DefaultBackgroundColor = Color.White;
@@ -33,15 +36,37 @@
             webView1.Size = new Size(800, 450);
             webView1.TabIndex = 0;
             webView1.ZoomFactor = 1D;
-
+            // 
+            // btnNaruto
+            // 
+            btnNaruto.Location = new Point(60, 26);
+            btnNaruto.Name = "btnNaruto";
+            btnNaruto.Size = new Size(75, 23);
+            btnNaruto.TabIndex = 1;
+            btnNaruto.Text = "button1";
+            btnNaruto.UseVisualStyleBackColor = true;
+            btnNaruto.Click += btnNaruto_Click;
+            // 
+            // btnAttackOnTitan
+            // 
+            btnAttackOnTitan.Location = new Point(169, 26);
+            btnAttackOnTitan.Name = "btnAttackOnTitan";
+            btnAttackOnTitan.Size = new Size(75, 23);
+            btnAttackOnTitan.TabIndex = 2;
+            btnAttackOnTitan.Text = "button2";
+            btnAttackOnTitan.UseVisualStyleBackColor = true;
+            btnAttackOnTitan.Click += btnAttackOnTitan_Click;
+            // 
             // Form1
+            // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(btnAttackOnTitan);
+            Controls.Add(btnNaruto);
             Controls.Add(webView1);
             Name = "Form1";
             Text = "Animex Player";
-
             ((System.ComponentModel.ISupportInitialize)webView1).EndInit();
             ResumeLayout(false);
         }
@@ -49,5 +74,7 @@
         #endregion
 
         private Microsoft.Web.WebView2.WinForms.WebView2 webView1;
+        private Button btnNaruto;
+        private Button btnAttackOnTitan;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.IO;
 using System.Windows.Forms;
 
@@ -7,10 +7,13 @@ namespace animex_player
     public partial class Form1 : Form
     {
         string htmlPath;
+
         public Form1()
         {
             InitializeComponent();
+            this.Load += Form1_Load;
         }
+
         private async void Form1_Load(object sender, EventArgs e)
         {
             await webView1.EnsureCoreWebView2Async(null);
@@ -18,11 +21,31 @@ namespace animex_player
             webView1.CoreWebView2.Settings.AreDevToolsEnabled = false;
             webView1.CoreWebView2.Settings.AreDefaultScriptDialogsEnabled = false;
 
-            webView1.Source = new Uri("https://www.youtube.com");
-        }
-        private void webView1_Click(object sender, EventArgs e)
-        {
+            // Ruta del HTML local
+            htmlPath = Path.Combine(Application.StartupPath, "player.html");
 
+            webView1.CoreWebView2.Navigate(htmlPath);
+        }
+
+        // 🔥 MÉTODO CORRECTO PARA LLAMAR JS
+        private async void LoadYouTubeVideo(string videoId)
+        {
+            if (webView1.CoreWebView2 != null)
+            {
+                await webView1.CoreWebView2.ExecuteScriptAsync(
+                    $"loadVideo('{videoId}')"
+                );
+            }
+        }
+
+        private void btnNaruto_Click(object sender, EventArgs e)
+        {
+            LoadYouTubeVideo("J---aiyznGQ");
+        }
+
+        private void btnAttackOnTitan_Click(object sender, EventArgs e)
+        {
+            LoadYouTubeVideo("MGRm4IzK1SQ");
         }
     }
 }
